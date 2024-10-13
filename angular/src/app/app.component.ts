d60a931e6d6ad21cb01e55bd47a1085633b31d2b
lisambert <tony.lisambert@quizzbox.com>
2024-10-13 14:35:04 +0200

    🥚 🐣 [FRONT] add fist components for 'add' and 'users' routes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, UsersListComponent, AddUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'users', component: UsersListComponent },
+    { path: 'add', component: AddUserComponent },
+];
