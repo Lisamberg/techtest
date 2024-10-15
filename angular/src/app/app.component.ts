c698f09929a679de45858eb907ab60d674d6d423
lisambert <tony.lisambert@quizzbox.com>
2024-10-15 21:30:25 +0200

    🔥 [FRONT] remove useless components and make routes simpler

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { UsersListComponent } from './users-list/users-list.component';
-import { AddUserComponent } from './add-user/add-user.component';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+    RouterLink,
+    RouterLinkActive,
+    RouterModule,
+    RouterOutlet,
+} from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+
 import { UserService } from './services/user.service';
+
+import { User } from './models/user.model';
+
 import { UserAdderButtonComponent } from './user-adder-button/user-adder-button.component';
+
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -13,15 +25,41 @@ import { UserAdderButtonComponent } from './user-adder-button/user-adder-button.
         RouterOutlet,
         RouterLink,
         RouterLinkActive,
-        UsersListComponent,
         UserAdderButtonComponent,
-        AddUserComponent,
         HttpClientModule,
+        MatListModule,
+        MatTableModule,
+        MatPaginatorModule,
+        RouterModule,
+        UserAdderButtonComponent,
     ],
     providers: [UserService],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
 })
-export class AppComponent {
-    title = 'ng-app';
+export class AppComponent implements OnInit, AfterViewInit {
+    @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+    displayedColumns: string[] = ['No', 'ID', 'lastName', 'firstName'];
+    private usersSubject = new BehaviorSubject<User[]>([]);
+    dataSource = new MatTableDataSource<User>();
+
+    constructor(private userService: UserService) {}
+
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator as MatPaginator;
+    }
+
+    ngOnInit(): void {
+        this.userService.getUsers().subscribe({
+            next: (data) => {
+                this.usersSubject.next(data);
+            },
+            error: (err) => console.error('Error fetching users:', err),
+        });
+
+        this.usersSubject.subscribe((users) => {
+            this.dataSource.data = users;
+        });
+    }
 }
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-import { UsersListComponent } from './users-list/users-list.component';
-import { AddUserComponent } from './add-user/add-user.component';
+import { AddingUserComponent } from './adding-user/adding-user.component';
 
 export const routes: Routes = [
-    { path: 'users', component: UsersListComponent },
-    { path: 'add', component: AddUserComponent },
+    {
+        path: 'add',
+        component: AddingUserComponent,
+    },
 ];
