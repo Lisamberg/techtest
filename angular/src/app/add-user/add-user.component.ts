cb288498abafa40526d10166a5b24488121a3041
lisamberg <tony.lisambert@icloud.com>
2024-10-20 10:31:28 +0200

    🔥 [FRONT] remove useless ondestroy

--- angular/src/app/add-user/add-user.component.ts
+++ angular/src/app/add-user/add-user.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalAddingUserOverviewComponent } from '../modal-adding-user-overview/modal-adding-user-overview.component';
 
@@ -8,15 +7,10 @@ import { ModalAddingUserOverviewComponent } from '../modal-adding-user-overview/
     imports: [],
     template: `<div></div>`,
 })
-export class AddUserComponent implements OnDestroy {
-    destroy = new Subject<any>();
+export class AddUserComponent {
     currentDialog = null;
 
     constructor(private dialog: MatDialog) {
         this.dialog.open(ModalAddingUserOverviewComponent);
-    }
-
-    ngOnDestroy() {
-        this.destroy.next(true);
     }
 }
